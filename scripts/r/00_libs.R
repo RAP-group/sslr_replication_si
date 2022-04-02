02509876d758dfae9e2a63ce627bb8b17090939d
jvcasillas <joseph.casillas@rutgers.edu>
2022-04-02 10:41:21 -0400

    scripts organization

---
+++ scripts/r/00_libs.R
@@ -0,0 +1,22 @@
+# Load libraries --------------------------------------------------------------
+
+# System
+library("here")
+library("fs")
+library("readr")
+library("tidyr")
+library("dplyr")
+library("forcats")
+
+# Plots
+library("ggplot2")
+library("ggdist")
+
+# Models
+library("brms")
+library("bayestestR")
+
+# Power
+library("esc")
+
+# -----------------------------------------------------------------------------
